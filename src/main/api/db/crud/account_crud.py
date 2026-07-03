b4bbe7745d4821bdd8bfa63e0c1a5c1f16ecc44c
rulkabek-web <rulkabek@gmail.com>
2026-07-03 13:15:36 +0400

    после 2 видео все работает

---
+++ src/main/api/db/crud/account_crud.py
@@ -0,0 +1,14 @@
+from src.main.api.db.models.account_table import Account
+from sqlalchemy.orm import Session
+
+class AccountCrudDb:
+    @staticmethod
+    def get_account_by_id(db: Session, account_id: int) -> Account | None:
+        return db.query(Account).filter_by(id=account_id).first()
+
+    @staticmethod
+    def delete_account(db: Session, account_id: int) -> None:
+        account = db.query(Account).filter_by(id=account_id).first()
+        if account:
+            db.delete(account)
+            db.commit()
---
+++ src/main/api/db/models/account_table.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, Float, String, Boolean, DateTime, ForeignKey
+from src.main.api.db.base import Base
+
+
+class Account(Base):
+    __tablename__ = 'account'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    number = Column(String, unique=True, nullable=False)
+    balance = Column(Float, nullable=False)
+
+def __repr__(self):
+    return f"<Account(id={self.id}, user_id={self.user_id}, balance={self.balance}, number={self.number})>"
